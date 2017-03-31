650f1b33177def557b01aeab7719dea518acf776
Allan Pasco <ajpasco@gmail.com>
2017-03-31 18:03:16 -0400

    adding space to see if vs code will commit staged

--- macro-web/Program.cs
+++ macro-web/Program.cs
@@ -11,6 +11,7 @@ namespace macro_web
     {
         public static void Main(string[] args)
         {
+            
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
